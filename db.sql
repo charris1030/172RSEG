39b14dc56dc6ed928177d2f3bea35712b40f9d18
Chris Harris <cghar1030@brandeis.edu>
2017-06-06 22:02:58 -0400

    committing create table script

---
+++ db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE stored_images (             
+              id int(5) NOT NULL auto_increment,   
+              image blob,                          
+              PRIMARY KEY  (`id`)                   
+ );
