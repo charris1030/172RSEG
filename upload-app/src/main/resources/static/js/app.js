09b2aaa55169032eec5d2f03b79c4b1b5ef132d9
Z50BXH <brad.hardwick@pes-companies.com>
2017-06-19 09:33:52 -0400

    Updated link to post file. Modified UI slightly

--- upload-app/src/main/resources/static/js/app.js
+++ upload-app/src/main/resources/static/js/app.js
@@ -1,9 +1,11 @@
 var app = angular.module("app", ["angularFileUpload"]);
+
 app.controller("CreateCtrl", function(
   $scope,
   $timeout,
   $interval,
-  FileUploader
+  FileUploader,
+   $http
 ) {
   $scope.File = null;
   $scope.Model = {};
@@ -60,9 +62,21 @@ app.controller("CreateCtrl", function(
     { Display: " Wyoming", Value: "WY" }
   ];
 
-  var uploader = ($scope.uploader = new FileUploader({
-    url: "localhost:60965/documentation/index"
-  }));
+  $scope.Upload = function(F)
+  {
+    console.log(F);
+    $http({
+      method:'PUT',
+      headers: { "Content-Type": F.type },
+      url:"https://fileuploadapp-env.us-east-2.elasticbeanstalk.com/",
+      data: F.name
+    }).then(function(R){
+      console.log(R);
+    },function(E){console.log(E)});
+  }
+  var uploader = $scope.uploader =  new FileUploader({
+   
+  });
   uploader.filters.push({
     name: "customFilter",
     fn: function(item /*{File|FileLikeObject}*/, options) {
@@ -80,7 +94,7 @@ app.controller("CreateCtrl", function(
   uploader.onAfterAddingFile = function(fileItem) {
     console.info("onAfterAddingFile", fileItem);
     $scope.File = fileItem._file;
-    
+    console.log(uploader.queue);
   };
   uploader.onSuccessItem = function(fileItem, response, status, headers) {
     console.log("Success");
@@ -109,3 +123,53 @@ app.controller("Ctrl", function($scope, $timeout, $interval) {
     }
   };
 });
+
+ /**
+    * The ng-thumb directive
+    * @author: nerv
+    * @version: 0.1.2, 2014-01-09
+    */
+    app.directive('ngThumb', ['$window', function($window) {
+        var helper = {
+            support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+            isFile: function(item) {
+                return angular.isObject(item) && item instanceof $window.File;
+            },
+            isImage: function(file) {
+                var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        };
+
+        return {
+            restrict: 'A',
+            template: '<canvas/>',
+            link: function(scope, element, attributes) {
+                if (!helper.support) return;
+
+                var params = scope.$eval(attributes.ngThumb);
+
+                if (!helper.isFile(params.file)) return;
+                if (!helper.isImage(params.file)) return;
+
+                var canvas = element.find('canvas');
+                var reader = new FileReader();
+
+                reader.onload = onLoadFile;
+                reader.readAsDataURL(params.file);
+
+                function onLoadFile(event) {
+                    var img = new Image();
+                    img.onload = onLoadImage;
+                    img.src = event.target.result;
+                }
+
+                function onLoadImage() {
+                    var width = params.width || this.width / this.height * params.height;
+                    var height = params.height || this.height / this.width * params.width;
+                    canvas.attr({ width: width, height: height });
+                    canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+                }
+            }
+        };
+    }]);
